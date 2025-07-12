e8f4eafef01b96d712eb41ba46f13b830720b2cc
kushalraga <kushalraga20@gmail.com>
2025-07-13 00:20:58 +0530

    first template added

--- bookshelf/library/views.py
+++ bookshelf/library/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to the library homepage')
+    return render(request, 'library/home.html')
