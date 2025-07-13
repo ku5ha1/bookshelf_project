ed665b9a748421ead1e30d4d5bc5e22206e101c7
kushalraga <kushalraga20@gmail.com>
2025-07-13 23:10:02 +0530

    views and style added

--- bookshelf/library/urls.py
+++ bookshelf/library/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:book_id>', views.book_detail, name='book_detail'),
+    path('books/toggle/<int:book_id>', views.toggle_read_status, name='toggle_read_status'),
+    path('books/edit/<int:book_id>', views.edit_book, name='edit_book')
 ]
--- bookshelf/library/views.py
+++ bookshelf/library/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import Book 
+from .forms import BookForm
 
 # Create your views here.
 def home(request):
     return render(request, 'library/home.html')
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'library/book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'library/add_book.html', {'form': form})
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'library/book_detail.html', {'book': book})
+
+def toggle_read_status(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.is_read = not book.is_read
+    book.save()
+    return redirect('book_detail', book_id=book_id)
+
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', book_id=book_id)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library/edit_book.html', {'form': form, 'book': book})
