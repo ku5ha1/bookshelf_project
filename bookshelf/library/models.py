2beb0c071674d1147d12b001830e6d559ecd2d1b
kushalraga <kushalraga20@gmail.com>
2025-07-13 21:32:13 +0530

    book, category models added

--- bookshelf/library/models.py
+++ bookshelf/library/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)  
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    is_read = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
