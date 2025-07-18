ddfb7daceadada1a7f0eb5cdc282ba30fc00382f
kushalraga <kushalraga20@gmail.com>
2025-07-18 11:58:35 +0530

    api_views added

--- bookshelf/library/urls.py
+++ bookshelf/library/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
+from .api_views import BookList, BookDetail, CategoryView
+
  
 urlpatterns = [
     path('home/', views.home, name='home'),
@@ -7,5 +9,8 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:book_id>', views.book_detail, name='book_detail'),
     path('books/toggle/<int:book_id>', views.toggle_read_status, name='toggle_read_status'),
-    path('books/edit/<int:book_id>', views.edit_book, name='edit_book')
+    path('books/edit/<int:book_id>', views.edit_book, name='edit_book'),
+    path('api/books', BookList.as_view()),
+    path('api/books/<int:pk>', BookDetail.as_view()),
+    path('api/categories', CategoryView.as_view())
 ]
